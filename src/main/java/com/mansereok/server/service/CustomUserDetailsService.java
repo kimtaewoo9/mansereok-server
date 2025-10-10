82f83fb23924b2e650fd8a27aef22163fa22a566
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-10-10 18:25:35 +0900

    feat: 로그인 오류 수정

--- src/main/java/com/mansereok/server/service/CustomUserDetailsService.java
+++ src/main/java/com/mansereok/server/service/CustomUserDetailsService.java
@@ -23,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByUsername(email)
+		User user = userRepository.findByEmail(email)
 			.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
 
 		return new CustomUserPrincipal(user);
