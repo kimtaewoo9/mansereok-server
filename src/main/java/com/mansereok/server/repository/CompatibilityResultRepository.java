9da7e882f4f2b338428fec3a6a11d0287e3e4146
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-10-24 14:40:01 +0900

    feat: 궁합 저장 로직 추가

--- src/main/java/com/mansereok/server/repository/CompatibilityResultRepository.java
+++ src/main/java/com/mansereok/server/repository/CompatibilityResultRepository.java
@@ -1,11 +1,13 @@
 package com.mansereok.server.repository;
 
 import com.mansereok.server.entity.CompatibilityResult;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompatibilityResultRepository extends JpaRepository<CompatibilityResult, Long> {
 
-
+	// 사주를 검색을 할때 .. 일반 사주랑, 궁합 사주, 삼각 관계 사주 따로 구헤야할듯 ..
+	List<CompatibilityResult> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
