574c735a17f895552459310edbad3506f77ca254
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-11-12 14:36:10 +0900

    feat: @Retryable로 502 에러 발생시 재시도

--- src/main/java/com/mansereok/server/MansereokApplication.java
+++ src/main/java/com/mansereok/server/MansereokApplication.java
@@ -4,11 +4,11 @@ import com.mansereok.server.global.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties.class)
-@Retryable
+@EnableRetry
 public class MansereokApplication {
 
 	public static void main(String[] args) {
