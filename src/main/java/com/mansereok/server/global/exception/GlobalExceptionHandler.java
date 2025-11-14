8a747e58939d2ed94029c7eb1d2edbc1be384a43
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-11-14 16:48:54 +0900

    feat: swagger 문서에서 globalExceptionHandler 숨김 처리

--- src/main/java/com/mansereok/server/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mansereok/server/global/exception/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.mansereok.server.global.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
-import javax.naming.AuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,7 @@ import org.springframework.web.client.HttpStatusCodeException;
 
 @Slf4j
 @RestControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
 
 	/**
