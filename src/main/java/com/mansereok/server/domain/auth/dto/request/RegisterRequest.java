b18baed260e4daf4d08cb3c66cae7b3f5197de3b
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-12-09 18:29:26 +0900

    feat: 회원가입 폼 조건 수정

--- src/main/java/com/mansereok/server/domain/auth/dto/request/RegisterRequest.java
+++ src/main/java/com/mansereok/server/domain/auth/dto/request/RegisterRequest.java
@@ -15,15 +15,15 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest {
 
 	@NotBlank(message = "사용자명은 필수입니다")
-	@Size(min = 3, max = 20, message = "사용자명은 3-20자 사이여야 합니다")
+	@Size(min = 3, max = 20, message = "사용자명은 3-20자 사이여야 합니다.")
 	private String name;
 
 	@NotBlank(message = "이메일은 필수입니다")
-	@Email(message = "올바른 이메일 형식이어야 합니다")
+	@Email(message = "올바른 이메일 형식을 입력해주세요.")
 	private String email;
 
 	@NotBlank(message = "비밀번호는 필수입니다")
-	@Size(min = 6, message = "비밀번호는 최소 6자 이상이어야 합니다")
+	@Size(min = 6, message = "비밀번호는 최소 6자 이상이어야 합니다.")
 	private String password;
 
 	private LocalDate birthDate;
